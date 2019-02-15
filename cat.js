02d902bb5288c77f09d0d640ca63c0107edc34ba
levanthang <levanthangthxd@gmail.com>
2019-02-15 18:05:41 +0700

    Fix: missing cat.js and mouse.js

--- cat.js
+++ cat.js
@@ -1,3 +1,11 @@
-function(){
-	
+function Cat(){
+
 }
+module('module name', {
+  setup: function() {
+    // setup for module name 
+  },
+  teardown: function() {
+    //teardown for module name
+  }
+});
